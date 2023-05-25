3a5024ce61bf87bc08c4a8905490ddc6cf273259
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-25 12:54:46 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
 import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
 
   return (
     <>
-    <Navbar/>
+      <Routes>
+        
+        <Route path='/' element={} />
+
+      </Routes>
     </>
   )
 }
