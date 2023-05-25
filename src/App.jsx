43abca929f17fa5142997349d8631b06034da70a
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-25 13:10:28 +0600

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
-import Navbar from './components/Navbar'
-import { Route, Routes } from 'react-router-dom';
-import Master from './components/layout/Maser';
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Master from "./components/layout/Master";
 
 function App() {
-
   return (
     <>
       <Routes>
-
-        <Route  element={Master}>
-
-          <Route/>
-
+        <Route path="/" element={<Master />}>
+          <Route index element={<Home />} />
+          <Route path="/book"  element={<Home />} />
+          <Route path="/about"  element={<Home />} />
+          <Route path="/contact"  element={<Home />} />
+        </Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
