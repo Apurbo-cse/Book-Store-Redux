52e7bfd39b16783d8c230e8f64d9bd3331030f41
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-26 09:14:10 +0600

    Create fetchBooks.jsx

---
+++ src/redux/feature/book/thunk/fetchBooks.jsx
@@ -0,0 +1,9 @@
+import { loaded } from "../action";
+
+
+const fetchBooks = async (dispatch) => {
+    const res = await fetch('http://localhost:9000/books');
+    const books = await res.json();
+    dispatch(loaded(books))
+}
+export default fetchBooks;
