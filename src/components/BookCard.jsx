fc34c0f2e9a08e9023bb89991c6c660cdf7c7a63
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-27 00:02:02 +0600

    Update BookCard.jsx

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 export const BookCard = ({ data }) => {
+  const renderRatingStars = () => {
+    const stars = [];
+
+    for (let i = 0; i < data.rating; i++) {
+      stars.push(<i key={i} className="fa-solid fa-star text-warning"></i>);
+    }
+
+    for (let i = data.rating; i < 5; i++) {
+      stars.push(<i key={i} className="fa-regular fa-star"></i>);
+    }
+
+    return stars;
+  };
+
   return (
     <>
       <div className="col-md-6 mb-3">
@@ -10,16 +24,21 @@ export const BookCard = ({ data }) => {
           </div>
           <div className="px-2 pt-1 col-md-8">
             <div className="d-flex justify-content-between pb-2">
-              <div>{data.featured && <><span className=' fw-bold'>Features</span></>}</div>
-              <div className='d-flex justify-content-end px-2 pt-1'> <i className="cp fa-regular fa-pen-to-square me-2 text-info"></i> <i className="cp fa-solid fa-trash-can text-danger"></i></div>
+              <div>{data.featured && <><span className='fw-bold'>Features</span></>}</div>
+              <div className='d-flex justify-content-end px-2 pt-1'>
+                <i className="cp fa-regular fa-pen-to-square me-2 text-info"></i>
+                <i className="cp fa-solid fa-trash-can text-danger"></i>
+              </div>
             </div>
             <h5>{data?.name}</h5>
             <small>{data?.author}</small>
-            <p><i className="fa-solid fa-star text-warning"></i> <i className="fa-regular fa-star"></i></p>
+            <p>
+              {renderRatingStars()}
+            </p>
             <p>BDT {data?.price}</p>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
