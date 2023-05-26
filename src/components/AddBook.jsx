e52e7ab0b47261045757b7e726ced98cefab5fcf
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-26 22:35:42 +0600

    Update AddBook.jsx

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -4,66 +4,112 @@ import addBook from "../redux/feature/book/thunk/addBook";
 
 const AddBook = () => {
     const dispatch = useDispatch();
-    const [ input, setInput] = useState('');
-    const [ author, setAuthor] = useState('');
-    const [ img_url, setImgUrl] = useState('');
-    const [ price, setPrice] = useState('');
-    const [ rating, setRating] = useState('');
+    const [input, setInput] = useState('');
+    const [author, setAuthor] = useState('');
+    const [img_url, setImgUrl] = useState('');
+    const [price, setPrice] = useState('');
+    const [rating, setRating] = useState('');
     const [featured, setFeatured] = useState(false);
+    const [inputError, setInputError] = useState(false);
+    const [authorError, setAuthorError] = useState(false);
+    const [img_urlError, setImgUrlError] = useState(false);
+    const [priceError, setPriceError] = useState(false);
+    const [ratingError, setRatingError] = useState(false);
 
     const handelInput = (e) => {
         setInput(e.target.value);
+        setInputError(false);
     }
+
     const handelAuth = (e) => {
         setAuthor(e.target.value);
+        setAuthorError(false);
     }
+
     const handelImgUrl = (e) => {
         setImgUrl(e.target.value);
+        setImgUrlError(false);
     }
+
     const handelPrice = (e) => {
         setPrice(e.target.value);
+        setPriceError(false);
     }
+
     const handelRating = (e) => {
         setRating(e.target.value);
+        setRatingError(false);
     }
+
     const handleFeaturedChange = (e) => {
         setFeatured(e.target.checked);
-      };
+    };
 
     const handelSubmit = (e) => {
         e.preventDefault();
-        console.log(featured);
-        dispatch(addBook(input,author,img_url,price,rating,featured));
-        setInput("");
-        setAuthor("");
-        setImgUrl("");
-        setPrice("");
-        setRating("");
-        setFeatured("");
+
+        let hasError = false;
+
+        if (input.trim() === '') {
+            setInputError(true);
+            hasError = true;
+        }
+
+        if (author.trim() === '') {
+            setAuthorError(true);
+            hasError = true;
+        }
+
+        if (img_url.trim() === '') {
+            setImgUrlError(true);
+            hasError = true;
+        }
+
+        if (price.trim() === '') {
+            setPriceError(true);
+            hasError = true;
+        }
+
+        if (rating.trim() === '') {
+            setRatingError(true);
+            hasError = true;
+        }
+
+        if (hasError) {
+            return;
+        }
+
+        dispatch(addBook(input, author, img_url, price, rating, featured));
+        setInput('');
+        setAuthor('');
+        setImgUrl('');
+        setPrice('');
+        setRating('');
+        setFeatured(false);
     }
+
     return (
         <>
             <form onSubmit={handelSubmit} className="br bg-light rounded-3 p-2 pt-0">
                 <h4>Create New Book</h4>
                 <div className="form-group">
-                    <label for="exampleInputEmail1">Book Name</label>
-                    <input type="text" value={input} onChange={handelInput} className="form-control" />
-                    {/* <small id="emailHelp" className="form-text text-muted mb-2">
-                        We'll never share your email with anyone else.
-                    </small> */}
+                    <label htmlFor="exampleInputEmail1">Book Name</label>
+                    <input type="text" value={input} onChange={handelInput} className={`form-control ${inputError ? 'is-invalid' : ''}`} />
+                    {inputError && <div className="invalid-feedback">Please enter a book name</div>}
                 </div>
                 <div className="form-group">
-                    <label for="exampleInputEmail1">Author</label>
-                    <input type="text" value={author} onChange={handelAuth} className="form-control" />
+                    <label htmlFor="exampleInputEmail1">Author</label>
+                    <input type="text" value={author} onChange={handelAuth} className={`form-control ${authorError ? 'is-invalid' : ''}`} />
+                    {authorError && <div className="invalid-feedback">Please enter an author name</div>}
                 </div>
 
                 <div className="form-group">
-                    <label for="exampleInputEmail1">Image Url</label>
+                    <label htmlFor="exampleInputEmail1">Image Url</label>
                     <input type="text" value={img_url} onChange={handelImgUrl} className="form-control" />
                 </div>
                 <div className="row">
                     <div className="form-group col-md-6">
-                        <label for="inputEmail4">Price</label>
+                        <label htmlFor="inputEmail4">Price</label>
                         <input type="text" value={price} onChange={handelPrice} className="form-control" placeholder="0" />
                     </div>
                     <div className="form-group col-md-6">
@@ -73,8 +119,8 @@ const AddBook = () => {
                 </div>
 
                 <div className="form-check my-2">
-                    <input type="checkbox" checked={featured} onChange={handleFeaturedChange} className="form-check-input" id="exampleCheck1"/>
-                    <label className="form-check-label" for="exampleCheck1">
+                    <input type="checkbox" checked={featured} onChange={handleFeaturedChange} className="form-check-input" id="exampleCheck1" />
+                    <label className="form-check-label" htmlFor="exampleCheck1">
                         This is a featured book
                     </label>
                 </div>
