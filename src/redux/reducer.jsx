ac6102518b59b2fec449abc12f2e8c3b4e1470f9
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-26 08:51:51 +0600

    Create reducer.jsx

---
+++ src/redux/reducer.jsx
@@ -0,0 +1,15 @@
+import { LOADED } from "./actionType";
+
+
+
+const reducer = (state , action) => {
+    switch (key) {
+        case LOADED:
+            return action.payload;
+    
+        default:
+            return state
+    }
+}
+
+export default reducer;
