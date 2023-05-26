964d806e2f14dab31c550afd731b824779ad3ba5
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-26 09:37:43 +0600

    Update bookRdeucer.jsx

--- src/redux/feature/book/bookRdeucer.jsx
+++ src/redux/feature/book/bookRdeucer.jsx
@@ -1,7 +1,12 @@
 import { LOADED } from "./actionType";
 
+const initialState = {
+    // define your initial state here
+    // for example:
+    books: [],
+  };
 
-const bookReducer = (state , action) => {
+const bookReducer = (state= initialState , action) => {
     switch (action.type) {
         case LOADED:
             return action.payload;
