5d2d46027ba8df47823d2f2f321a99bcdcf8898e
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-26 12:09:27 +0600

    Update bookRdeucer.jsx

--- src/redux/feature/book/bookRdeucer.jsx
+++ src/redux/feature/book/bookRdeucer.jsx
@@ -1,10 +1,25 @@
 import { LOADED } from "./actionType";
 import initialState from "./initialState";
 
+const nextBookId = (books) => {
+    const maxId = books.reduce((maxId, data) => Math.max(data.id, maxId), -1);
+    return maxId + 1;
+}
+
 const bookReducer = (state=initialState , action) => {
     switch (action.type) {
         case LOADED:
             return action.payload;
+
+        case ADDED:
+            return [
+                ...state, 
+                {
+                    id: nextBookId(state),
+                    text: action.payload,
+                    completed: false
+                }
+            ]
     
         default:
             return state
