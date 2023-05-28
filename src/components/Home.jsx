6cb17bcee1602a9ceaace20be98d8f8c425da6f1
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-28 11:24:24 +0600

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -63,7 +63,40 @@ const Home = () => {
           </div>
         </div>
         <div className="col-md-3">
+<<<<<<< Updated upstream
           <AddBook />
+=======
+          <form className="shadow p-2 pt-0">
+            <h4>Create New Book</h4>
+            <div className="form-group">
+              <label for="exampleInputEmail1">Book Name</label>
+              <input type="text" className="form-control" />
+              {/* <small id="emailHelp" className="form-text text-muted mb-2">
+                We'll never share your email with anyone else.
+              </small> */}
+            </div>
+            <div className="form-group">
+              <label for="exampleInputEmail1">Author</label>
+              <input type="text" className="form-control" />
+            </div>
+
+            <div className="form-group">
+              <label for="exampleInputEmail1">Image Url</label>         
+              <input type="text" className="form-control" />
+            </div>
+
+            <div className="form-check my-2">
+              <input type="checkbox"  className="form-check-input" id="exampleCheck1"  />
+              <label className="form-check-label" for="exampleCheck1">
+                This is a featured book
+              </label>
+            </div>
+
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+>>>>>>> Stashed changes
         </div>
       </div>
     </div>
